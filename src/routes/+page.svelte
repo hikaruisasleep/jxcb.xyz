286d170f7014ad941b1dba047f92f057b9092e72
Ben Wu <ben@jxcb.xyz>
2023-05-11 17:42:37 +0700

    hero so far

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
+<div class="bg gradient fixed top-0 left-0 -z-50 h-screen w-full" />
 <section class="hero h-screen">
-	<div class="container bg-zinc-300 min-h-full min-w-full grid place-items-center relative -z-50">
+	<div class="container min-h-full min-w-full">
 		<header
-			class="hidden lg:flex flex-initial justify-between items-center px-24 py-3 fixed top-0 left-0 max-h-min min-w-full"
+			class="hidden lg:flex flex-initial justify-between items-center px-24 py-3 fixed top-0 left-0 max-h-min min-w-full z-10"
 		>
 			<p class="text-3xl font-bold align-middle">jxcb</p>
 			<nav>
@@ -11,25 +12,70 @@
 				</ul>
 			</nav>
 		</header>
-		<div class="w-full flex flex-col items-center lg:items-start lg:max-w-[50%] lg:-translate-x-20">
-			<div>
-				<p class="text-lg translate-y-14 translate-x-10">hi, I'm</p>
-				<h1 class="text-9xl text-transparent font-extrabold main-title">jxcb</h1>
+
+		<div class="grid place-items-center grid-flow-row lg:grid-flow-col h-screen w-full">
+			<div
+				class="w-full flex flex-col items-center lg:items-start lg:max-w-[50%] lg:-translate-x-12 max-h-screen"
+			>
+				<p
+					class="text-xl translate-y-8 -translate-x-11 lg:text-2xl lg:translate-x-16 lg:translate-y-16 animate-[slideUpFade_1s_ease-out_both]"
+				>
+					hi, I'm
+				</p>
+				<h1
+					class="text-8xl lg:text-[12rem] text-transparent font-extrabold main-title font-[Prompt] animate-[slideUpFade_1s_ease-out_1s_forwards]"
+				>
+					jxcb
+				</h1>
 			</div>
-			<span
-				class="gradient hidden bg-slate-800 lg:block lg:max-w-screen w-screen min-h-[200vw] fixed top-0 left-0 -z-10"
-			/>
+			<div class="text-lg">
+				<ul
+					class="flex flex-col flex-initial justify-center items-end text-right align-middle lg:translate-x-8"
+				>
+					<li>junior fullstack dev</li>
+					<li>motion designer</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </section>
+
+<svg class="h-0 w-0">
+	<filter id="grain">
+		<feTurbulence type="turbulence" baseFrequency="0.65" />
+	</filter>
+</svg>
 
 <style lang="scss">
 	h1.main-title {
 		-webkit-text-stroke: 2px black;
 	}
 
-	span.gradient {
-		filter: blur(5rem);
-		transform: translate(calc(-50% + 5rem), -25%);
+	.bg {
+		z-index: -999;
+		&::before,
+		&::after {
+			position: absolute;
+			left: 0;
+			top: 0;
+			content: '';
+			width: 100%;
+			height: 100%;
+			z-index: 9999;
+			opacity: 0.5;
+		}
+		&::before {
+			filter: url(#grain);
+		}
+
+		&.gradient {
+			background: linear-gradient(to right, rgb(30 41 59) 0, rgb(212 212 216) 15% 100%);
+		}
+
+		@media (min-width: 1024px) {
+			&.gradient {
+				background: linear-gradient(to right, rgb(30 41 59) 0 47.5%, rgb(212 212 216) 100%);
+			}
+		}
 	}
 </style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,23 @@ const config = {
 			height: {
 				screen: '100svh'
 			},
+			maxHeight: {
+				screen: '100svh'
+			},
 			fontFamily: {
 				sans: ['"Wix Madefor Display"', '"Helvetica Neue"', '"Noto Sans"', 'sans-serif']
+			},
+			keyframes: {
+				slideUpFade: {
+					'0%': {
+						transform: 'translateY(1rem)',
+						opacity: '0'
+					},
+					'100%': {
+						transform: 'translateY(0)',
+						opacity: '1'
+					}
+				}
 			}
 		}
 	},
