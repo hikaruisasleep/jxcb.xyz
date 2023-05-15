b45c0c20f55f2e3a26d40430ce04861742ce418c
Ben Wu <benayaj.2004@gmail.com>
2023-05-15 13:39:58 +0700

    proper head titles with slug at the end

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import '$lib/styles/scss.scss';
 	import '$lib/styles/tailwind.postcss';
+	import { page } from '$app/stores';
 </script>
+
+<svelte:head>
+	<title>
+		jxcb
+		{$page.url.pathname.split('/').at(-1) ? `| ${$page.url.pathname.split('/').at(-1)}` : ''}
+	</title>
+</svelte:head>
 
 <slot />
