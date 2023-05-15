3a8d33041a5140b8218ea73feef0edfefc13e9be
Ben Wu <benayaj.2004@gmail.com>
2023-05-15 13:38:03 +0700

    header title is a link

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,5 @@
 <header class="w-full shadow-md flex flex-initial justify-between items-center p-3">
-	<p class="text-3xl font-bold align-middle">jxcb</p>
+	<p class="text-3xl font-bold align-middle"><a href="/">jxcb</a></p>
 	<nav>
 		<ul class="text-lg flex flex-row justify-center items-center gap-2">
 			<li><a href="/">home</a></li>
