0a2fe051ca3552672f8ee838f5059b117003b37e
Ben Wu <ben@jxcb.xyz>
2023-06-14 12:09:19 +0700

    mdsvex ig

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
@@ -13,7 +17,8 @@ const config = {
 			scss: {
 				prependData: '@use "src/lib/styles/variables.scss" as *;'
 			}
-		})
+		}),
+		mdsvex(mdsvexConfig)
 	],
 
 	kit: {
