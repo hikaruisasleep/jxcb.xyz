3dcbb2a3fff8eaa4ba1626380461d6e2a9ab8dc4
Ben Wu <ben@jxcb.xyz>
2023-06-15 11:03:38 +0700

    katex stuff (working!! wow)

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,10 +1,14 @@
 import { defineMDSveXConfig as defineConfig } from 'mdsvex';
+import math from 'remark-math';
+import math_katex from 'rehype-katex-svelte';
 
 const config = defineConfig({
 	extensions: ['.svx', '.md', '.svelte.md'],
 	smartypants: {
 		dashes: 'oldschool'
-	}
+	},
+	remarkPlugins: [math],
+	rehypePlugins: [math_katex]
 });
 
 export default config;
--- src/routes/(sections)/kreideprinz/blog/+layout@.svelte
+++ src/routes/(sections)/kreideprinz/blog/+layout@.svelte
@@ -2,15 +2,22 @@
 	import '$lib/styles/scss.scss';
 	import '$lib/styles/tailwind.postcss';
 
-	import { page } from '$app/stores';
-
 	import Nav from '$lib/components/kreideprinz/Nav.svelte';
 </script>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"
+		integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X"
+		crossorigin="anonymous"
+	/>
+</svelte:head>
 
 <div class="container h-[100dvh] min-w-full px-4 bg-gruvbg">
 	<header class="flex flex-row flex-wrap justify-between items-center py-2 text-gray-200">
 		<h1 class="text-3xl font-extrabold">
-			<a href="/kreideprinz"> kreideprinz blog </a>
+			<a href="/kreideprinz/blog"> kreideprinz blog </a>
 		</h1>
 		<Nav />
 	</header>
